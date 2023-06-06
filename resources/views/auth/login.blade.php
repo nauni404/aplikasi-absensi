7880ecc27a4b76b573568845d083eebf62da5bea
nauni404 <naufalnurf10@gmail.com>
2023-06-06 09:25:40 +0700

    Merubah tampilan login dan footer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
             </form>
         </div>
     </div>
-    <div class="mt-5 text-muted text-center">
+    {{-- <div class="mt-5 text-muted text-center">
         Don't have an account? <a href="register">Create now</a>
-    </div>
+    </div> --}}
 @endsection
